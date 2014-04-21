f8c60c994659fbfd3ae6434e5f6f9627107cc1e2
SatrioDwiPrabowo <sdprabowo77@gmail.com>
2014-04-21 22:20:09 +0700

    Seraphic App About

---
+++ src/com/kelvinvalencio/About/MainActivity.java
@@ -0,0 +1,51 @@
+//this is my source code for About Seraphic ROM apps
+//Authors : Kelvin Valencio
+//twitter : http://twitter.com/kelvin_valencio
+//facebook : http://facebook.com/kelvin.valencio.7
+package com.kelvinvalencio.About;
+
+import java.util.List;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.ComponentName;
+import android.content.Context;
+
+
+public class MainActivity extends Activity {
+
+MediaPlayer mp;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mp=MediaPlayer.create(MainActivity.this,R.raw.backgroundsong);
+		mp.setLooping(true);
+		mp.start();
+	}
+
+	
+	@Override
+	protected void onPause() {
+		if (this.isFinishing()){
+			mp.stop();
+		}
+		Context context = getApplicationContext();
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		List<RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+		if (!taskInfo.isEmpty()){
+			ComponentName topActivity = taskInfo.get(0).topActivity;
+			if (!topActivity.getPackageName().equals(context.getPackageName())) {
+				mp.stop();
+			}
+			else{
+		}
+	
+	
+	}
+super.onPause();
+}
+}
